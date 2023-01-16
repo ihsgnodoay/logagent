daa4eecb164cc706dff7039f7c9b06e62951738d
dyihs <sydbgx@gmail.com>
2023-01-16 15:17:32 +0800

    feat: 加载配置文件，取消固定写法

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
     "fmt"
     "github.com/dyihs/logagent/kafka"
     "github.com/dyihs/logagent/taillog"
+    "gopkg.in/ini.v1"
+    "os"
     "time"
+)
+
+var (
+    cfg, err = ini.Load("./config/config.ini")
 )
 
 func run() {
@@ -14,7 +20,7 @@ func run() {
         select {
         case line := <-taillog.ReadChan():
             // 2. 发送到kafka
-            kafka.SendToKafka("web-log", line.Text)
+            kafka.SendToKafka(cfg.Section("kafka").Key("topic").String(), line.Text)
         default:
             time.Sleep(time.Second)
         }
@@ -22,8 +28,21 @@ func run() {
 }
 
 func main() {
+    // 0. 加载配置文件
+    // cfg, err := ini.Load("./config/config.ini")
+    if err != nil {
+        fmt.Printf("Fail to read file: %v\n", err)
+        os.Exit(1)
+    }
+    // address := cfg.Section("kafka").Key("address").String()
+    // topic := cfg.Section("kafka).Key("address").String()
+    // fileName := cfg.Section("taillog").Key("path").String()
+    fmt.Println("address:", cfg.Section("kafka").Key("address").String())
+    fmt.Println("topic:", cfg.Section("kafka").Key("topic").String())
+    fmt.Println("filename:", cfg.Section("taillog").Key("filename").String())
+
     // 1. 初始化kafka连接
-    err := kafka.Init([]string{"127.0.0.1:9092"})
+    err = kafka.Init([]string{cfg.Section("kafka").Key("address").String()})
     if err != nil {
         fmt.Printf("init kafka failed：err%v\n", err)
         return
@@ -31,7 +50,7 @@ func main() {
     fmt.Println("init kafka success.")
 
     // 2. 打开日志文件准备收集日志
-    err = taillog.Init("./my.log")
+    err = taillog.Init(cfg.Section("taillog").Key("filename").String())
     if err != nil {
         fmt.Printf("init taillog failed,err:%v\n", err)
         return
