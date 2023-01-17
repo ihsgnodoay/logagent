af15d44eb9138f1495428d3d4b05b8af24fca411
dyihs <sydbgx@gmail.com>
2023-01-17 21:31:04 +0800

    v0.0.1: 从etcd中获取配置信息

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -1,14 +1,25 @@
 package etcd
 
 import (
+    "context"
+    "encoding/json"
     "fmt"
     clientv3 "go.etcd.io/etcd/client/v3"
     "time"
 )
 
+var (
+    cli *clientv3.Client
+)
+
+type LogEntry struct {
+    Path  string `json:"path"`
+    Topic string `json:"topic"`
+}
+
 // Init etcd 初始化
 func Init(addr string, timeout time.Duration) (err error) {
-    _, err = clientv3.New(clientv3.Config{
+    cli, err = clientv3.New(clientv3.Config{
         Endpoints:   []string{"127.0.0.1:2379"},
         DialTimeout: 5 * time.Second,
     })
@@ -16,6 +27,28 @@ func Init(addr string, timeout time.Duration) (err error) {
     if err != nil {
         fmt.Printf("connect to etcd failed, err:%v\n", err)
         return
+    }
+    return
+}
+
+// GetConf 根据 key 从etcd中获取配置项
+func GetConf(key string) (logEntryCOnf []*LogEntry, err error) {
+    // get
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    resp, err := cli.Get(ctx, key)
+    cancel()
+    if err != nil {
+        fmt.Printf("get from etcd failed, err:%v\n", err)
+        return
+    }
+
+    for _, ev := range resp.Kvs {
+        err = json.Unmarshal(ev.Value, &logEntryCOnf)
+        if err != nil {
+            fmt.Printf("Unmarshal etcd conf failed, err:%v\n", err)
+            return
+        }
+        fmt.Printf("%s:%s\n", ev.Key, ev.Value)
     }
     return
 }
---
+++ etcd/put/put.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    clientv3 "go.etcd.io/etcd/client/v3"
+    "time"
+)
+
+func main() {
+    cli, err := clientv3.New(clientv3.Config{
+        Endpoints:   []string{"127.0.0.1:2379"},
+        DialTimeout: 5 * time.Second,
+    })
+    if err != nil {
+        fmt.Printf("connect to etcd failed, err:%v\n", err)
+        return
+    }
+    fmt.Println("connect to etcd success")
+    defer cli.Close()
+
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    value := `[{"path":"/tmp/nginx.log","topic":"web-log"},{"path":"/tmp/redis.log","topic":"redis-log"}]`
+    _, err = cli.Put(ctx, "/etcd_conf", value)
+    cancel()
+    if err != nil {
+        fmt.Printf("put to etcd failed, err:%v\n", err)
+        return
+    }
+}
